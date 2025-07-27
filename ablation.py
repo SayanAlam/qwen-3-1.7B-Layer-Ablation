bf8f071345b91812376804f689eb114ef2b76db1
SayanAlam <99359691+SayanAlam@users.noreply.github.com>
2025-07-27 22:38:55 +0530

    Update ablation.py

--- ablation.py
+++ ablation.py
@@ -174,7 +174,7 @@ def main():
       "If 10% of x is equal to 25% of y, what is x in terms of y?",
       "What is the smallest prime number that is the sum of two other prime numbers?",
       "How many prime numbers are there between 50 and 100?",
-      "Find the number of trailing zeros in 100!"
+      "Find the number of trailing zeros in 100!",
     ]
 
 
@@ -235,7 +235,7 @@ def main():
         "Write a blog post about interesting facts about the Dutch language. Italicize at least 2 sections in your answer with markdown, i.e. *italic text*.",
         "Write a journal entry about stress management. Your entire response should contain less than 6 sentences.",
         "Write an itinerary for a trip to India in Shakespearean style. You are not allowed to use any commas in your response.",
-        "Write a resume for a fresh high school graduate seeking their first job. Make sure to include at least one placeholder represented by square brackets, such as [address]."
+        "Write a resume for a fresh high school graduate seeking their first job. Make sure to include at least one placeholder represented by square brackets, such as [address].",
         "I am planning a trip to Japan, and I would like thee to write an itinerary for my journey in a Shakespearean style. You are not allowed to use any commas in your response.",
     ]
 
