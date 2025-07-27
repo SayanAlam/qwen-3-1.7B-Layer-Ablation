dd9a26b3d53506b18097abd25153ddcfd3b64407
SayanAlam <99359691+SayanAlam@users.noreply.github.com>
2025-07-28 05:06:18 +0530

    Update representationProbing.py

--- representationProbing.py
+++ representationProbing.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 import pandas as pd
 import numpy as np
 
@@ -248,16 +249,16 @@ table_data = {
 for i, X in enumerate(X_layers):
     X = X.numpy()
     y = np.array([1]*len(instruction_prompts)+[0]*len(reasoning_prompts))
+    X, y = shuffle(X, y, random_state=42)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y, random_state=42)
     clf = LogisticRegression(max_iter=1000)
 
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
+
     instr_acc = accuracy_score(y_test[y_test == 1], y_pred[y_test == 1])
     reason_acc = accuracy_score(y_test[y_test == 0], y_pred[y_test == 0])
-
-
     acc = accuracy_score(y_test, y_pred)
 
     table_data["Layer"].append(i)
